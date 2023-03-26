e1f32e092f2f0d552c316f9e9494db6296833d1e
Muhammad-Jawwad <muhammadjawwad417@gmail.com>
2023-03-26 18:45:20 +0500

    Commiting updated code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRouter = require("./api/user/user.router");
-// const facultyRouter = require("./api/faculty/faculty.router");
+const facultyRouter = require("./api/faculty/faculty.router");
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/user", userRouter);
-// app.use("/api/faculty", facultyRouter);
+app.use("/api/faculty", facultyRouter);
 
 
 app.listen(process.env.APP_PORT, () => {
