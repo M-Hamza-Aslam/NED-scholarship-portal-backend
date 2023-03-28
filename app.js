66c67d0cd404ac2979b716ddd7ed61483b70f4de
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2023-03-28 18:50:44 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const userRouter = require("./api/user/user.router");
-const facultyRouter = require("./api/faculty/faculty.router");
+const express = require("express");
+const bodyParser = require("body-parser");
+const userRoutes = require("./api/routes/user");
+const facultyRoutes = require("./api/routes/faculty");
 
 const app = express();
 
@@ -11,10 +12,21 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/user", userRouter);
-app.use("/api/faculty", facultyRouter);
+app.use(userRoutes);
+app.use(facultyRoutes);
 
-
-app.listen(process.env.APP_PORT, () => {
-    console.log("server up and running on PORT :", process.env.APP_PORT);
-});
+// setting mongoose connection and starting server
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://MuhammadHamza:hamza12345@cluster0.fq85av6.mongodb.net/portalData?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+// app.listen(process.env.APP_PORT, () => {
+//   console.log("server up and running on PORT :", process.env.APP_PORT);
+// });
