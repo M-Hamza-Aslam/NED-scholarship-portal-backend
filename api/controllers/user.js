b69bcc088dd34c61ed38eb40143fce3aaec4eba2
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2023-03-30 00:02:35 +0500

    Update user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -8,30 +8,26 @@ const User = require("../modules/user");
 
 module.exports = {
   login: async (req, res) => {
+    try {
+      
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-    const { email, password } = req.body;
+    const { 
+      email, 
+      password 
+    } = req.body;
 
-    try {
-      // mongoose
-      // checking if user is existing
+      const userDetails = await User.findOne({ email: email });
 
-      // const resutl = User.findOne({email: email})
-
-      // const user = new User(email,password);
-      // const result = await user.save()
-
-      const user = await User.findOne({ email: email });
-
-      if (!user) {
+      if (!userDetails) {
         return res.status(401).json({
           error: "Invalid email",
         });
       }
 
-      const isMatch = await bcrypt.compare(password, user.password);
+      const isMatch = await bcrypt.compare(password, userDetails.password);
 
       if (!isMatch) {
         return res.status(401).json({
@@ -39,9 +35,12 @@ module.exports = {
         });
       }
 
-      const token = jwt.sign({ userId: user._id }, "your_secret_key_here");
+      const token = jwt.sign({ userId: userDetails._id.toString() }, "mysupersupersecretkey",{expiresIn:'1h'});
 
-      res.status(200).json({ token });
+      res.status(200).json({
+        message: "Login successful",
+        userId: userDetails._id.toString(),
+        token: token });
     } catch (error) {
       res.status(500).json({
         error: "Internal server error",
@@ -50,70 +49,80 @@ module.exports = {
   },
 
   signUp: async (req, res) => {
+    try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({
         errors: errors.array(),
       });
     }
-
-    const { firstname, lastname, email, password, phone_number } = req.body;
-
-    try {
-      const existingUser = await findUserByEmail(email);
+    const {
+      firstName, 
+      lastName, 
+      email, 
+      password, 
+      phoneNumber 
+    } = req.body;
+    
+      // Check if user already exists with this email 
+      const existingUser = await User.findOne({email: email})
 
       if (existingUser) {
         return res.status(409).json({
-          error: "User already exists",
-        });
+          message: "User with this email already exists",
+        })
       }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      const newUser = await createUser({
-        firstname,
-        lastname,
+      //Password encription
+      const hashedPassword = await bcrypt.hash(password, 10); 
+      
+      // Creating a new user document
+      const newUser = new User({
+        firstName,
+        lastName,
         email,
         password: hashedPassword,
-        phone_number,
+        phoneNumber,
       });
+      const result = await newUser.save();
 
+      // Returning success message
       res.status(201).json({
         message: "User created successfully",
+        userDetails: result,
       });
     } catch (error) {
-      res.status(500).json({
-        error: "Internal server error",
+      res.status(400).json({
+        message: error.message 
       });
     }
   },
-
-  protected: (req, res) => {
-    res.status(200).json({
-      message: `User ${req.userId} is authenticated`,
-    });
-  },
+  
+  // protected: (req, res, ) => {
+  //   res.status(200).json({
+  //     message: `User ${req.userId} is authenticated`,
+  //   });
+  // },
 };
 
 // Validation rules for login route
-exports.loginValidationRules = () => {
-  return [
-    body("email").isEmail().withMessage("Invalid email"),
-    body("password")
-      .isLength({ min: 8 })
-      .withMessage("Password must be at least 8 characters"),
-  ];
-};
+// exports.loginValidationRules = () => {
+//   return [
+//     body("email").isEmail().withMessage("Invalid email"),
+//     body("password")
+//       .isLength({ min: 8 })
+//       .withMessage("Password must be at least 8 characters"),
+//   ];
+// };
 
 // Validation rules for sign up route
-exports.signUpValidationRules = () => {
-  return [
-    body("firstname").not().isEmpty().withMessage("First name is required"),
-    body("lastname").not().isEmpty().withMessage("Last name is required"),
-    body("email").isEmail().withMessage("Invalid email"),
-    body("password")
-      .isLength({ min: 8 })
-      .withMessage("Password must be at least 8 characters"),
-    body("phone_number").isMobilePhone().withMessage("Invalid phone number"),
-  ];
-};
+// exports.signUpValidationRules = () => {
+//   return [
+//     body("firstname").not().isEmpty().withMessage("First name is required"),
+//     body("lastname").not().isEmpty().withMessage("Last name is required"),
+//     body("email").isEmail().withMessage("Invalid email"),
+//     body("password")
+//       .isLength({ min: 8 })
+//       .withMessage("Password must be at least 8 characters"),
+//     body("phone_number").isMobilePhone().withMessage("Invalid phone number"),
+//   ];
+// };
