8c6b3946959dc78bfbf923901570be97d2b435ce
Muhammad-Jawwad <muhammadjawwad417@gmail.com>
2023-04-10 14:04:46 +0500

    Your commit message here

---
+++ api/modules/scholarship.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const scholarshipSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  
+});
+
+module.exports = mongoose.model("Scholarship", scholarshipSchema);
