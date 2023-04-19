275cb7ac9ab3d566eb5c3c73197e926b80c10d0f
Muhammad-Jawwad <muhammadjawwad417@gmail.com>
2023-04-19 17:43:04 +0500

    userid updated

--- api/controllers/scholarship.js
+++ api/controllers/scholarship.js
@@ -1,6 +1,6 @@
 
 const {validationResult } = require("express-validator");
-
+const jwt = require('jsonwebtoken');
 const Scholarship = require("../modules/scholarship");
 const User = require("../modules/user");
 
@@ -124,12 +124,22 @@ module.exports = {
   },
   getAppliedScholarshipList: async (req, res) => {
     try{
+      const authHeader = req.headers['authorization'];
+      const token = authHeader && authHeader.split(' ')[1]; // extract token from header
+      if (!token) {
+        return res.status(401).json({ error: "Unauthorized" });
+      }
+
+      const decodedToken = jwt.decode(token, { complete: true });
+      const userId = decodedToken.payload.userId; // extract userId from token
+      
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
       }
+      
 
-      const user = await User.findById(req.userId);
+      const user = await User.findById(userId);
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
@@ -148,13 +158,22 @@ module.exports = {
   },
   appliedScholarship:async (req, res) => {
     try {
+      const authHeader = req.headers['authorization'];
+      const token = authHeader && authHeader.split(' ')[1]; // extract token from header
+      if (!token) {
+        return res.status(401).json({ error: "Unauthorized" });
+      }
+
+      const decodedToken = jwt.decode(token, { complete: true });
+      const userId = decodedToken.payload.userId; // extract userId from token
+      
       const { body } = req;
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
       }
       
-      const { userId, scholarshipId, status } = body;
+      const { scholarshipId} = body;
       
       const user = await User.findById(userId);
       
@@ -177,7 +196,7 @@ module.exports = {
       if (hasApproved) {
         return res.json({ error: "User already has an approved scholarship" });
       } else {
-        user.appliedScholarship.push({ scholarshipId, status });
+        user.appliedScholarship.push({ scholarshipId, status:"awaiting" });
         await user.save();
       
         return res.json({ success: "Applied scholarship added to user" });
