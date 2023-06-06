0476c088b80734bf66e8e7f9a87693dcd4ffa391
hamza <hamza.prolink@gmail.com>
2023-06-06 13:20:35 +0500

    bug updated

--- api/controllers/alumni.js
+++ api/controllers/alumni.js
@@ -14,132 +14,210 @@ const jwt = require("jsonwebtoken");
 // const PDFDocument = require("pdfkit");
 
 module.exports = {
-    //For alumni login
-    login: async (req, res) => {
-        try {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                return res.status(422).json({ errors: errors.array() });
-            }
-            const { email, password, userRole } = req.body;
+  //For alumni login
+  login: async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+      }
+      const { email, password, userRole } = req.body;
 
-            const alumniDetails = await Alumni.findOne({
-                email: email,
-                userRole: userRole,
-            });
+      const alumniDetails = await Alumni.findOne({
+        email: email,
+        userRole: userRole,
+      });
 
-            if (!alumniDetails) {
-                return res.status(401).json({
-                    message: "Alumni not found",
-                });
-            }
+      if (!alumniDetails) {
+        return res.status(401).json({
+          message: "Alumni not found",
+        });
+      }
 
-            const isMatch = await bcrypt.compare(password, alumniDetails.password);
+      const isMatch = await bcrypt.compare(password, alumniDetails.password);
 
-            if (!isMatch) {
-                return res.status(401).json({
-                    message: "Invalid password",
-                });
-            }
+      if (!isMatch) {
+        return res.status(401).json({
+          message: "Invalid password",
+        });
+      }
 
-            const token = jwt.sign(
-                {
-                    adminId: alumniDetails._id.toString(),
-                    userRole: alumniDetails.userRole,
-                    expiration: Date.now() + 3600000,
-                },
-                process.env.JWT_SecretKey,
-                { expiresIn: "1h" }
-            );
-            const alumniData = {
-                email: alumniDetails.email,
-                firstName: alumniDetails.firstName,
-                lastName: alumniDetails.lastName,
-                phoneNumber: alumniDetails.phoneNumber,
-                userRole: alumniDetails.userRole,
-            };
-            res.status(200).json({
-                message: "Login successful",
-                alumniDetails: alumniData,
-                adminId: alumniDetails._id.toString(),
-                token: token,
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({
-                message: "Internal server error",
-            });
+      const token = jwt.sign(
+        {
+          adminId: alumniDetails._id.toString(),
+          userRole: alumniDetails.userRole,
+          expiration: Date.now() + 3600000,
+        },
+        process.env.JWT_SecretKey,
+        { expiresIn: "1h" }
+      );
+      const alumniData = {
+        email: alumniDetails.email,
+        firstName: alumniDetails.firstName,
+        lastName: alumniDetails.lastName,
+        phoneNumber: alumniDetails.phoneNumber,
+        userRole: alumniDetails.userRole,
+        isVerified: alumniDetails.isVerified,
+      };
+      res.status(200).json({
+        message: "Login successful",
+        alumniDetails: alumniData,
+        adminId: alumniDetails._id.toString(),
+        token: token,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "Internal server error",
+      });
+    }
+  },
+  signUp: async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({
+          errors: errors.array(),
+        });
+      }
+      const { firstName, lastName, email, password, phoneNumber } = req.body;
+
+      // Check if user already exists with this email
+      const existingAlumni = await Alumni.findOne({ email: email });
+
+      if (existingAlumni) {
+        return res.status(409).json({
+          message: "Alumni with this email already exists",
+        });
+      }
+      //Password encription
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Creating a new alumni document
+      const newAlumni = new Alumni({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+        isVerified: false,
+        phoneNumber,
+        userRole: "alumni",
+        profileStatus: 0,
+        profileImg: "",
+        personalInfo: { isInitial: true },
+      });
+      const alumniDetails = await newAlumni.save();
+
+      //creating token
+      const token = jwt.sign(
+        {
+          alumniId: alumniDetails._id.toString(),
+          userRole: alumniDetails.userRole,
+          expiration: Date.now() + 3600000,
+        },
+        process.env.JWT_SecretKey,
+        { expiresIn: "1h" }
+      );
+
+      const alumniData = {
+        email: alumniDetails.email,
+        firstName: alumniDetails.firstName,
+        lastName: alumniDetails.lastName,
+        phoneNumber: alumniDetails.phoneNumber,
+        profileStatus: alumniDetails.profileStatus,
+        userRole: alumniDetails.userRole,
+        profileImg: alumniDetails.profileImg,
+      };
+
+      // Returning success message
+      res.status(201).json({
+        message: "Alumni created successfully, please verify your Email!",
+        alumniDetails: alumniData,
+        userId: alumniDetails._id.toString(),
+        token: token,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({
+        message: error.message,
+      });
+    }
+  },
+  emailVerification: async (req, res) => {
+    try {
+      //finding user
+      const userId = req.userId;
+      const alumni = await Alumni.findById(userId);
+      if (!alumni) {
+        return res.status(401).json({
+          message: "Alumni not found",
+        });
+      }
+      //creating 4-digit verification code
+      const emailVerificationCode = randomatic("0", 4);
+      //saving code in database
+      alumni.verificationCode = emailVerificationCode;
+      alumni.verificationCodeExpiration = new Date().getTime() + 900000;
+      await alumni.save();
+      //sending verification code inside email
+      transporter.sendMail(
+        {
+          to: alumni.email,
+          from: "hamza.prolink@gmail.com",
+          subject: "Email Verification",
+          html: `
+    <p>Have you requested for Email Verification ?</p>
+    <p>Here is your code: ${emailVerificationCode} </p>
+    <p>Remember it is valid for 15 minutes only!</p>
+`,
+        },
+        (err) => {
+          console.log(err);
         }
-    },
-    signUp: async (req, res) => {
-        try {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                return res.status(422).json({
-                    errors: errors.array(),
-                });
-            }
-            const { firstName, lastName, email, password, phoneNumber } = req.body;
+      );
+      res.status(201).json({
+        message: "Verification Code has been sent to Email!",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({
+        message: error.message,
+      });
+    }
+  },
+  verifyCode: async (req, res, next) => {
+    try {
+      const userId = req.userId;
+      const code = req.body.code;
+      //extracting user
+      const alumni = await Alumni.findById(userId);
+      if (!alumni) {
+        return res.status(401).json({
+          message: "Alumni not found",
+        });
+      }
+      //checking code
+      if (
+        code !== alumni.verificationCode ||
+        new Date().getTime() >= alumni.verificationCodeExpiration
+      ) {
+        return res.status(401).json({
+          message: "invalid verification code",
+        });
+      }
+      //updating databse
+      alumni.isVerified = true;
+      alumni.verificationCode = undefined;
+      alumni.verificationCodeExpiration = undefined;
+      await alumni.save();
 
-            // Check if user already exists with this email
-            const existingAlumni = await Alumni.findOne({ email: email });
-
-            if (existingAlumni) {
-                return res.status(409).json({
-                    message: "Alumni with this email already exists",
-                });
-            }
-            //Password encription
-            const hashedPassword = await bcrypt.hash(password, 10);
-
-            // Creating a new alumni document
-            const newAlumni = new Alumni({
-                firstName,
-                lastName,
-                email,
-                password: hashedPassword,
-                isVerified: false,
-                phoneNumber,
-                userRole: "alumni",
-                profileStatus: 0,
-                profileImg: "",
-                personalInfo: { isInitial: true },
-            });
-            const alumniDetails = await newAlumni.save();
-
-            //creating token
-            const token = jwt.sign(
-                {
-                    alumniId: alumniDetails._id.toString(),
-                    userRole: alumniDetails.userRole,
-                    expiration: Date.now() + 3600000,
-                },
-                process.env.JWT_SecretKey,
-                { expiresIn: "1h" }
-            );
-
-            const alumniData = {
-                email: alumniDetails.email,
-                firstName: alumniDetails.firstName,
-                lastName: alumniDetails.lastName,
-                phoneNumber: alumniDetails.phoneNumber,
-                profileStatus: alumniDetails.profileStatus,
-                userRole: alumniDetails.userRole,
-                profileImg: alumniDetails.profileImg,
-            };
-
-            // Returning success message
-            res.status(201).json({
-                message: "Alumni created successfully, please verify your Email!",
-                alumniDetails: alumniData,
-                userId: alumniDetails._id.toString(),
-                token: token,
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(400).json({
-                message: error.message,
-            });
-        }
-    },
+      res.status(201).json({
+        message: "Your account has been verified!",
+      });
+    } catch (error) {
+      res.status(400).json({
+        message: error.message,
+      });
+    }
+  },
 };
