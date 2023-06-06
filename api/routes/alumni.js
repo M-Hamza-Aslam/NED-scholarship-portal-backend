746bb74b2289432aecadfec49110e4bb6c1cc3fa
hamza <hamza.prolink@gmail.com>
2023-06-06 13:42:45 +0500

    added routes

--- api/routes/alumni.js
+++ api/routes/alumni.js
@@ -1,17 +1,21 @@
 const router = require("express").Router();
 const {
-    login,
-    signUp,
-    
+  login,
+  signUp,
+  emailVerification,
+  verifyCode,
 } = require("../controllers/alumni");
 
+const authenticateToken = require("../middlewares/isAuth");
+
 const {
-    validateLogin,
-    validateSignUp,
+  validateLogin,
+  validateSignUp,
 } = require("../../util/alumniInputValidation");
 
 router.post("/login", validateLogin, login);
 router.post("/signup", validateSignUp, signUp);
+router.get("/emailVerification", authenticateToken, emailVerification);
+router.post("/verifyCode", authenticateToken, verifyCode);
 
 module.exports = router;
-
